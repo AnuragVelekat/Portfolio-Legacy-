5be487b85a68e4c94d7d065d98c37bd060c79731
Anurag Velekat <120166854+An-rag02@users.noreply.github.com>
2023-09-30 19:55:49 +0530

    Adding project

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,10 +1,11 @@
 export default [
   {
-    title: "Project 1",
-    imgUrl: "https://placehold.co/600x400.png",
-    stack: ["tool1", "tool2", "tool3"],
+    title: "Auto ML",
+    imgUrl: "assets\\Streamlit_AutoML.png",
+    stack: ["Python", "Streamlit", "Machine Learning"],
     link: "", //link to project
-    description: "This project does this.",
+    description:
+      "This is an Auto ML application that lets the user upload a dataset for a classification problem. The app performs various tasks on the data including testing the dataset with multiple different Machine Learning models and lets the user download the best model based on accuracy score and various other classification metrics.",
   },
   {
     title: "Project 2",
